4464b0c4ef1ea6d58cee36020a59a0979f28dac8
James Kariuki <karyorkir@gmail.com>
2018-01-11 23:01:10 +0300

    [Bug #153728030] fix account tests
    This fixes all the account tests in the following test files;
    test_check_token.py
    test_login.py
    test_profile.py
    test_switch_team.py
    test_team_access_middleware.py

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -19,19 +19,21 @@ class LoginTestCase(TestCase):
         r = self.client.post("/accounts/login/", form)
         assert r.status_code == 302
 
-        ### Assert that a user was created
-
+        # Assert that a user was created
+        self.assertEqual(User.objects.count(), 1)
+        user = User.objects.get(email="alice@example.org")
+        self.assertEqual(user.email, "alice@example.org")
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
-        ### Assert contents of the email body
+        # Assert contents of the email body
+        self.assertIn('To log into healthchecks.io', mail.outbox[0].body, )
 
-        ### Assert that check is associated with the new user
+        # Assert that check is associated with the new user
+        check_assoc = Check.objects.get(code=check.code)
+        assert check_assoc.user
 
     def test_it_pops_bad_link_from_session(self):
         self.client.session["bad_link"] = True
         self.client.get("/accounts/login/")
         assert "bad_link" not in self.client.session
-
-        ### Any other tests?
-
