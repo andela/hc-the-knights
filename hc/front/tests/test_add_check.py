be81a4bd9d5a8e7e6309b070b18adf092c1d4da3
Basemera <phiona.basemera@andela.com>
2018-01-11 14:31:03 +0300

    chore #153728028 modify test_add_check.py

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -1,5 +1,6 @@
 from hc.api.models import Check
 from hc.test import BaseTestCase
+from django.contrib.auth.models import User
 
 
 class AddCheckTestCase(BaseTestCase):
@@ -13,11 +14,22 @@ class AddCheckTestCase(BaseTestCase):
 
     ### Test that team access works
 
+    # def test_team_access_works(self):
+    #     url = "/checks/add/"
+    #     self.client.login(username="alice@example.org", password="password")
+    #     r = self.client.post(url)
+    #     self.client.logout()
+    #     self.client.login(username="bob@example.org", password="password")
+    #     self.client.get()
+    #     self.assertRedirects(r, "/checks/")
+    #     assert Check.objects.count() == 1
+
     def test_team_access_works(self):
+        alice = User.objects.get(email="alice@example.org")
+        alice_bchecks = Check.objects.filter(user=alice).count()
         url = "/checks/add/"
-        self.client.login(username="alice@example.org", password="password")
-        r = self.client.post(url)
-        self.client.logout()
         self.client.login(username="bob@example.org", password="password")
-        self.assertRedirects(r, "/checks/")
-        assert Check.objects.count() == 1
+        self.client.post(url)
+        self.client.logout()
+        alice_achecks = Check.objects.filter(user=alice).count()
+        self.assertEqual(alice_achecks, (alice_bchecks + 1))
