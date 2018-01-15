998828a4c49094e50b49ab1b7bc612d1e70bf38b
Ssemaganda <ssemagandavictor@gmail.com>
2018-01-15 14:09:06 +0300

    #153728029 Add api functionality tests (#7)
    * Fixed django.url errors during migration
    
    * [Chore #153728029] Ignore .vscode
    
    * [CHORE #153728029] update admin and badge test modules
    
    * [chore #153728029] update list checks tests
    
    * [CHORE #153728029] update test_check_model.py
    
    * [CHORE #153728029] update test_create_check.py
    
    * [CHORE #153728029] update test_ensuretriggers.py
    
    * [chore #153728029] Add api functionality tests
    
    * [chore #153728029] Add api functionality tests
    
    * [chore #153728029] merge gitignore conflict
    
    * [CHORE #153728029] update admin and badge test modules
    
    * [chore #153728029] update list checks tests
    
    * [CHORE #153728029] update test_check_model.py
    
    * [CHORE #153728029] update test_create_check.py
    
    * [CHORE #153728029] update test_ensuretriggers.py
    
    * [chore #153728029] Add api functionality tests
    
    * [chore #153728029] fix merge conflict in list checks
    
    * #153728028 Add tests to front tests (#5)
    
    * modify test.py
    
    * modify test_add_channel.py
    
    * modify  requirements.txt
    
    * modify gitignore
    
    * modify test_add_channel.py file
    
    * [chore #2137219] Add frontend tests
    
    * [ch #[153728028]] Resolving merge conflicts
    
    * #154130089 Add Continuous Integration (#2)
    
    * Feature-[#154130089] add Coveralls Badge to app
    
    * Feature-[#154130089] add Cicle CI integration
    
    * Bug #154130089 add mock to requirements.txt
    
    * modify test.py
    
    * modify test_add_channel.py
    
    * modify gitignore
    
    * modify test_add_channel.py file
    
    * [chore #2137219] Add frontend tests
    
    * [chore #153728028] modify test_add_channel.py
    
    * [Fix #154167186] Replace django 1.10 with 1.11] (#3)
    
    * #154130050 heroku deployment (#4)
    
    * [CHORE]: Adding Procfile
    
    * [Chore #154065111] Fix the command in the procfile
    
    * [Chore #154065111] Heroku Deployment configurations
    
    * Update Procfile
    
    * Update runtime.txt
    
    * Update runtime.txt
    
    * Update runtime.txt
    
    * Update requirements.txt
    
    * [Chore #154065111] Heroku Deployment configurations
    
    * [BUG] Fix import errors
    
    This enables the app to start on local
    
    [Fixes #154065111] Deploy to Heroku
    
    * Update requirements.txt
    
    * Update Procfile
    
    * Update settings.py
    
    * modify test.py
    
    * modify test_add_channel.py
    
    * modify  requirements.txt
    
    * modify gitignore
    
    * modify test_add_channel.py file
    
    * [chore #2137219] Add frontend tests
    
    * modify test_add_channel.py
    
    * [chore #153728028] modify test_add_channel.py
    
    * chore #153728028 modify test_add_check.py
    
    * chore #153728028 modify test_add_check.py
    
    * [Bug #153728030] fix account tests (#6)
    
    This fixes all the account tests
    
    * [CHORE #153728029] Solve merge conflict in .gitignore
    
    * [Chore #153728029] Fix merge conflict in .gitignore
    
    * [CHORE #153728029] update admin and badge test modules
    
    * [chore #153728029] update list checks tests
    
    * [CHORE #153728029] update test_check_model.py
    
    * [CHORE #153728029] update test_create_check.py
    
    * [CHORE #153728029] update test_ensuretriggers.py
    
    * [chore #153728029] Add api functionality tests
    
    * [Chore #153728029] Fix merge conflict in .gitignore
    
    * [Chore #153728029] Fix merge conflict in .gitignore
    
    * [Chore #153728029] Fix merge conflict in .gitignore
    
    * [Chore #153728029] Fix merge conflict in .gitignore

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -7,6 +7,7 @@ from hc.test import BaseTestCase
 
 
 class ListChecksTestCase(BaseTestCase):
+    URL = "/api/v1/checks/"
 
     def setUp(self):
         super(ListChecksTestCase, self).setUp()
@@ -33,24 +34,56 @@ class ListChecksTestCase(BaseTestCase):
 
     def test_it_works(self):
         r = self.get()
-        ### Assert the response status code
-
+        # Assert the response status code
+        self.assertEqual(r.status_code, 200)
         doc = r.json()
         self.assertTrue("checks" in doc)
 
         checks = {check["name"]: check for check in doc["checks"]}
-        ### Assert the expected length of checks
-        ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
-        ### last_ping, n_pings and pause_url
+
+        # Assert the expected length of checks
+        # Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
+        # last_ping, n_pings and pause_url
+        ping_endpoint = 'http://localhost:8000/ping/'
+        alice_2_code = str(self.a2.code)
+        alice_2_ping_url = ping_endpoint + alice_2_code
+        pauseurl = '/api/v1/checks/'
+        alice_2_pause_url = ping_endpoint + pauseurl + alice_2_code
+
+        # Alice 1 tests
+        self.assertTrue(0 < len(checks['Alice 1']['name']) <= 100)
+        self.assertEqual(checks['Alice 1']['timeout'], 3600)
+        self.assertEqual(checks['Alice 1']['grace'], 900)
+        self.assertTrue(alice_2_ping_url)
+        self.assertEqual(checks['Alice 1']['status'], self.a1.status)
+        self.assertTrue(checks['Alice 1']['pause_url'])
+        self.assertTrue(checks['Alice 1']['n_pings'], self.a1.n_pings)
+        self.assertTrue(checks['Alice 1']['last_ping'],
+                        self.a1.to_dict()['last_ping'])
+
+        # Alice 2 tests
+        self.assertTrue(0 < len(checks['Alice 2']['name']) <= 100)
+        self.assertEqual(checks['Alice 2']['timeout'], 86400)
+        self.assertEqual(checks['Alice 2']['grace'], 3600)
+        self.assertTrue(alice_2_ping_url)
+        self.assertEqual(checks['Alice 2']['status'], self.a2.status)
+        self.assertTrue(checks['Alice 2']['pause_url'])
+        self.assertEqual(checks['Alice 2']['n_pings'], self.a2.n_pings)
+        self.assertEqual(checks['Alice 2']['last_ping'],
+                         self.a2.to_dict()['last_ping'])
 
     def test_it_shows_only_users_checks(self):
         bobs_check = Check(user=self.bob, name="Bob 1")
         bobs_check.save()
-
         r = self.get()
         data = r.json()
         self.assertEqual(len(data["checks"]), 2)
         for check in data["checks"]:
             self.assertNotEqual(check["name"], "Bob 1")
 
-    ### Test that it accepts an api_key in the request
+    # Test that it accepts an api_key in the request
+    def test_it_accepts_api_key(self):
+
+        r = self.client.generic('GET', self.URL, json.dumps({"api_key": "abc"}),
+                                content_type="application/json")
+        self.assertEqual(r.status_code, 200)
--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -12,7 +12,8 @@ class PauseTestCase(BaseTestCase):
         r = self.client.post(url, "", content_type="application/json",
                              HTTP_X_API_KEY="abc")
 
-        ### Assert the expected status code and check's status
+        # Assert the expected status code and check's status
+        self.assertEqual(r.status_code, 200)
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -24,4 +25,23 @@ class PauseTestCase(BaseTestCase):
 
         self.assertEqual(r.status_code, 400)
 
-        ### Test that it only allows post requests
+        # Test that it only allows post requests
+    def test_it_only_allows_post_requests(self):
+        check = Check(user=self.bob, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+        r = self.client.post(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 400)
+        r = self.client.get(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.put(
+            url, '', content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.delete(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
