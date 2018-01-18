acbe240e26ae1dbef3f39135c45c8aa562877e93
Anyama Richard <richard@Anyamas-MacBook-Pro.local>
2018-01-18 22:35:26 +0300

    [CHORE #153728033] added email configuration in settings

--- hc/settings.py
+++ hc/settings.py
@@ -146,6 +146,12 @@ STATICFILES_FINDERS = (
 COMPRESS_OFFLINE = True
 
 EMAIL_BACKEND = "djmail.backends.default.EmailBackend"
+DJMAIL_REAL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_POST = 587
+EMAIL_HOST_USER =  "hctheknights@gmail.com"
+EMAIL_HOST_PASSWORD = "hctk12345"
+EMAIL_USE_TLS = True
 
 # Slack integration -- override these in local_settings
 SLACK_CLIENT_ID = None
