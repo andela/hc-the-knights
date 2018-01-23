fd85ef082c44b47711f18510cbdb56b68d23091d
Thegaijin <ssemagandavictor@gmail.com>
2018-01-23 14:08:08 +0300

    [bug #154466617] update heroku settings.

--- hc/settings.py
+++ hc/settings.py
@@ -86,8 +86,7 @@ TEST_RUNNER = 'hc.api.tests.CustomRunner'
 # install requirements.txt and do manage.py runserver and it works
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':   './hc.sqlite',
+        'ENGINE':   'django.db.backends.postgresql',
     }
 }
 
