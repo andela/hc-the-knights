37c1104c5dd920b4631b281ead824252d7bad004
Thegaijin <ssemagandavictor@gmail.com>
2018-01-30 10:47:58 +0300

    [feature #153728040] Update siteroot url in settings

--- hc/settings.py
+++ hc/settings.py
@@ -20,7 +20,7 @@ HOST = "localhost"
 SECRET_KEY = "---"
 DEBUG = True
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
-                 'hc-the-knight.herokuapp.com',]
+                 'hc-the-knight.herokuapp.com', ]
 DEFAULT_FROM_EMAIL = 'healthchecks@example.org'
 USE_PAYMENTS = False
 
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "hc-faqs.herokuapp.com"
+SITE_ROOT = "hc-the-knight.herokuapp.com"
 
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
